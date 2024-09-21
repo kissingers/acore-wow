5a20fce0718117548f4e8f4bd8f1caf21d8063b3
kissingers <kissinger@foxmail.com>
2024-09-21 20:12:13 +0800

    Revert "修改为GM外普通用户也可以召唤邮件"
    This reverts commit 3dab87ea02127557261bbe191f9d8e679b02c803.

--- src/server/game/Handlers/MailHandler.cpp
+++ src/server/game/Handlers/MailHandler.cpp
@@ -41,7 +41,7 @@ bool WorldSession::CanOpenMailBox(ObjectGuid guid)
         if (_player->GetSession()->GetSecurity() < SEC_MODERATOR)
         {
             LOG_ERROR("network.opcode", "{} attempt open mailbox in cheating way.", _player->GetName());
-            return true;
+            return false;
         }
     }
     else if (guid.IsGameObject())
