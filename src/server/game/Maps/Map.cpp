838e38fd5843c81f0208e3c92152698cb5e43d85
Andrew <47818697+Nyeriah@users.noreply.github.com>
2025-10-13 09:56:17 -0300

    fix(Core/Map): Fix zone player count update on player removal (#23211)

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -433,7 +433,9 @@ void Map::UpdatePlayerZoneStats(uint32 oldZone, uint32 newZone)
         else
             --oldZoneCount;
     }
-    ++_zonePlayerCountMap[newZone];
+
+    if (newZone != MAP_INVALID_ZONE)
+        ++_zonePlayerCountMap[newZone];
 }
 
 void Map::Update(const uint32 t_diff, const uint32 s_diff, bool  /*thread*/)
@@ -707,8 +709,8 @@ struct ResetNotifier
 
 void Map::RemovePlayerFromMap(Player* player, bool remove)
 {
-    // Before leaving map, update zone/area for stats
-    player->UpdateZone(MAP_INVALID_ZONE, 0);
+    UpdatePlayerZoneStats(player->GetZoneId(), MAP_INVALID_ZONE);
+
     player->getHostileRefMgr().deleteReferences(true); // pussywizard: multithreading crashfix
 
     player->RemoveFromWorld();
