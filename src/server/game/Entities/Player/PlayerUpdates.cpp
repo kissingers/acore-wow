cd1e4446c8651f5c79b6d1cf7f01cfdfc8a77506
隐如仙 <18535853+PkllonG@users.noreply.github.com>
2026-09-05 18:09:24 +0800

    fix(Core/Player): Fixed cross-zone weather residual bug (Burning Flatlands example). (#27116)

--- src/server/game/Entities/Player/PlayerUpdates.cpp
+++ src/server/game/Entities/Player/PlayerUpdates.cpp
@@ -1312,7 +1312,8 @@ void Player::UpdateZone(uint32 newZone, uint32 newArea, bool force)
         return;
 
     if (sWorld->getBoolConfig(CONFIG_WEATHER))
-        GetMap()->GetOrGenerateZoneDefaultWeather(newZone);
+        if (!GetMap()->GetOrGenerateZoneDefaultWeather(newZone))
+            Weather::SendFineWeatherUpdateToPlayer(this);
 
     GetMap()->SendZoneDynamicInfo(newZone, this);
 
